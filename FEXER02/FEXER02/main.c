0ae9394514dfacf07fd37358d22b715fc300be6d
Rimar Navaja <rimarnavaja19@gmail.com>
2022-12-14 14:20:10 +0800

    Update main.c

--- FEXER02/FEXER02/main.c
+++ FEXER02/FEXER02/main.c
@@ -1,27 +1,24 @@
-//
-//  main.c
-//  FEXER02
-//
-//  Created by PROGRAMMING on 28/11/2022.
-//  Copyright © 2022 CC-Comprog11. All rights reserved.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    int a, b, i, Q, R;
-    printf("input 1st number:");
-    scanf("%d",&a);
-    printf("input 2nd number:");
-    scanf("%d",&b);
-    i=0;
-    do {
-        a=a-b;
-        i++;
-        Q=i;
-        R=a;
-    }while (a>=b);
-    printf("The Quotient is: %d",Q);
-    printf("\nThe Remainder is: %d",R);
-    return 0;
+int main() {
+    
+    int a, b;
+    printf("Enter two positive integers:");
+    scanf("%d %d", &a, &b);
+    
+    if (b > 0){
+        int q = 0;
+        int r = a;
+        while (r >= b) {
+         r-= b;
+         q++; 
+        }
+
+    printf("Quotient: %d\n", q);
+    printf("Remainder: %d\n", r);
+  } 
+
+  return 0;
 }
+
+
