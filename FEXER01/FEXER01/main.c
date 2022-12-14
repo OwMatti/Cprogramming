b974df29093af3f575e2ff1bf56e5b56557c9a24
Rimar Navaja <rimarnavaja19@gmail.com>
2022-12-14 16:13:25 +0800

    Update main.c

--- FEXER01/FEXER01/main.c
+++ FEXER01/FEXER01/main.c
@@ -1,63 +1,38 @@
-//
-//  main.c
-//  FEXER01
-//
-//  Created by PROGRAMMING on 07/12/2022.
-//  Copyright © 2022 CC-Comprog11. All rights reserved.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    int num, SN, LN, ctr, TAN=0, TANN=0, TCNN=0, TAPN=0, TCN50_100=0;
+    int num,S=0,L=0, ctr, total_num=0, total_neg=0, count_neg=0, total_pos=0,  between=0;
+
     for (ctr=1; ctr <= 10; ctr++)
     {
-        printf("Integer#%d:", ctr);
+        printf("Integer-%d:", ctr);
         scanf("%d",&num);
-        TAN=TAN+num;
-        if (ctr==1)
-        {
-            SN=num;
+
+       
+        total_num += num;
+        if (num < S || ctr == 1) S = num;
+        if (num > L || ctr == 1) L = num;
+
+       
+        if (num < 0) {
+            total_neg += num;
+            count_neg++;
+        } else {
+            total_pos += num;
         }
-        else if (num<SN)
-        {
-            SN=num;
-        }
-        ///
-        if (ctr!=1)
-        {
-            if (num > LN )
-            {
-                LN = num;
-            }
-        }
-        else
-        {
-            LN = num;
-        }
-        /////
-        if (num<0)
-        {
-            TANN=TANN+num;
-            TCNN=TCNN+1;
-        }
-        else
-        {
-            TAPN=TAPN+num;
-        }
-        ///
-        /////
-        if (num>50 && num<100)
-        {
-            TCN50_100=TCN50_100+1;
-        }
+
+       
+        if (num > 50 && num < 100)  
+        between++;
     }
-    printf("a.The Total of All Numbers: %d",TAN);
-    printf("\nb.The Largest Number is: %d",LN);
-    printf("\nc.The Smallest Number is: %d",SN);
-    printf("\nd.The Total of all Positive Numbers: %d",TAPN);
-    printf("\ne.The Total of all Negative Numbers: %d",TANN);
-    printf("\nf.The Total Count of Negative Numbers: %d",TCNN);
-    printf("\ng.The Total  Count of Numbers between 50 and 100: %d",TCN50_100);
+
+    printf("a.The Total of All Numbers: %d",total_num);
+    printf("\nb.The Largest Number is: %d",L);
+    printf("\nc.The Smallest Number is: %d",S);
+    printf("\nd.The Total of all Positive Numbers: %d",total_pos);
+    printf("\ne.The Total of all Negative Numbers: %d",total_neg);
+    printf("\nf.The Total Count of Negative Numbers: %d",count_neg);
+    printf("\ng.The Total  Count of Numbers between 50 and 100: %d", between);
+
     return 0;
 }
