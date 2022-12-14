10ea8144eeeed543462ec5d4d301a1ca5fc1cfac
Rimar Navaja <rimarnavaja19@gmail.com>
2022-12-14 15:46:12 +0800

    Update main.c

--- FEXER04/FEXER04/main.c
+++ FEXER04/FEXER04/main.c
@@ -1,53 +1,71 @@
-//
-//  main.c
-//  FEXER04
-//
-//  Created by PROGRAMMING on 12/12/2022.
-//  Copyright © 2022 CC-Comprog11. All rights reserved.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    int num, i, inpn=0;
-    for (i=1; num<2; i++)
+int main(int argc, const char *argv[])
+{
+
+int num, i, ctr = 0;
+for (i = 1; num < 2; i++)
+{
+    printf("\nInput number 0 to 2: ");
+    scanf("%d", &num);
+
+    if (num == 0)
     {
-        printf("\nInput number's 0 to 2: ");
-        scanf("%d",&num);
-        inpn = inpn * num;
-        if (inpn * num + num == 0)
+        printf("Terminated by the opponent");
+        break;
+
+    }
+    else if (num == 2)
+    {
+        printf("End of the Game");
+        break;
+    }
+    else if (i == 1)
+    {
+        printf("First Blood!");
+        ctr++;
+    }
+    else
+    {
+
+        ctr++;
+        switch (ctr)
         {
-            printf("Terminated by a Hero");
+            case 1:
+                printf("Killed a Hero");
+                break;
+            case 2:
+                printf("Double Kill!");
+                break;
+            case 3:
+                printf("Killing Spree!");
+                break;
+            case 4:
+                printf("Dominating!");
+                break;
+            case 5:
+                printf("Mega Kill!");
+                break;
+            case 6:
+                printf("Unstoppable!");
+                break;
+            case 7:
+                printf("Wicked Sick!");
+                break;
+            case 8:
+                printf("Monster Kill!");
+                break;
+            case 9:
+                printf("God Like!");
+                break;
         }
-        else if (num == 2)
+        if (ctr >= 10)
         {
-            printf("End of the Game");
-        }
-        else if (i == 1)
-        {
-            printf("First Blood");
-            inpn+=1;
-        }
-        else
-        {
-            inpn++;
-            switch (inpn)
-            {
-                case 1: printf("Terminating a Hero"); break;
-                case 2: printf("Double Kill"); break;
-                case 3: printf("Terminating Spree"); break;
-                case 4: printf("dominating"); break;
-                case 5: printf("Mega Kill"); break;
-                case 6: printf("Unstappoable"); break;
-                case 7: printf("Wicked Sick"); break;
-                case 8: printf("Monster Kill"); break;
-                case 9: printf("God Like"); break;
-            }
-            if (inpn>=10)
-            {
-                printf("Beyond God Like");
-            }
+            printf("Beyond God Like!");
         }
     }
-    return 0;
+}
+
+
+return 0;
 }
